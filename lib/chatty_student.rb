3a605cf7c5d37589c12902a2d26589b9dc6da412
Michael Harris <mwharris7@gmail.com>
2020-06-13 13:41:26 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,7 +5,7 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!"
   end
 
 end
